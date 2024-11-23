0dea3ec5b0cc30be45e70208d0ab251183c44ccc
Yeali Kim <ykim229@my.bcit.ca>
2024-11-22 21:05:37 -0800

    Remove unused method

--- src/TrashPanda/Mode.java
+++ src/TrashPanda/Mode.java
@@ -3,8 +3,7 @@ package TrashPanda;
 import javafx.scene.canvas.GraphicsContext;
 
 public interface Mode {
-    int getMazeSize();
     Maze createMaze();
-    void applyEffects(GraphicsContext gc);
+    void applyEffect(Player player);
     String getName();
 }
