63a9002afb1d068236cd3ceea2f81a0502b411c0
Yeali Kim <ykim229@my.bcit.ca>
2024-11-27 22:17:57 -0800

    Write javadoc and adhere to checkstyle

--- src/TrashPanda/ImageLoader.java
+++ src/TrashPanda/ImageLoader.java
@@ -2,26 +2,71 @@ package TrashPanda;
 
 import javafx.scene.image.Image;
 
-import java.util.*;
+import java.util.Objects;
 
-class ImageLoader {
-    public static final Image[] frontImages = new Image[4];
-    public static final Image[] leftImages = new Image[4];
-    public static final Image[] rightImages = new Image[4];
-    public static final Image[] backImages = new Image[4];
-    public static Image wall;
-    public static Image floor;
-    public static Image neighbor;
+/**
+ * Loads and initializes image assets from resources folder to be used in game.
+ *
+ * @author Yeali Kim
+ * @version 2024
+ */
+public final class ImageLoader {
+    /**
+     * Number of images per character direction array.
+     */
+    static final int ARRAY_SIZE = 4;
+    /**
+     * Image representing a wall tile.
+     **/
+    static Image wall;
+    /**
+     * Image representing a floor tile.
+     **/
+    static Image floor;
+    /**
+     * Image representing a neighbor.
+     **/
+    static Image neighbor;
+    /**
+     * Array of front images for character.
+     **/
+    static final Image[] FRONT_IMAGES = new Image[ARRAY_SIZE];
+    /**
+     * Array of left images for character.
+     **/
+    static final Image[] LEFT_IMAGES = new Image[ARRAY_SIZE];
+    /**
+     * Array of right images for character.
+     **/
+    static final Image[] RIGHT_IMAGES = new Image[ARRAY_SIZE];
+    /**
+     * Array of back images for character.
+     **/
+    static final Image[] BACK_IMAGES = new Image[ARRAY_SIZE];
 
-    public static void loadImages() {
-        for (int i = 0; i < 4; i++) {
-            frontImages[i] = new Image(Objects.requireNonNull(ImageLoader.class.getResourceAsStream("/images/front" + i + ".png")));
-            leftImages[i] = new Image(Objects.requireNonNull(ImageLoader.class.getResourceAsStream("/images/left" + i + ".png")));
-            rightImages[i] = new Image(Objects.requireNonNull(ImageLoader.class.getResourceAsStream("/images/right" + i + ".png")));
-            backImages[i] = new Image(Objects.requireNonNull(ImageLoader.class.getResourceAsStream("/images/back" + i + ".png")));
+    private ImageLoader() {
+    }
+
+    /**
+     * Loads images for the four directional arrays for character and specific images for
+     * wall, floor, and neighbor from the images folder.
+     */
+    static void loadImages() {
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            FRONT_IMAGES[i] = new Image(Objects.requireNonNull(
+                    ImageLoader.class.getResourceAsStream("/images/front" + i + ".png")));
+            LEFT_IMAGES[i] = new Image(Objects.requireNonNull(
+                    ImageLoader.class.getResourceAsStream("/images/left" + i + ".png")));
+            RIGHT_IMAGES[i] = new Image(Objects.requireNonNull(
+                    ImageLoader.class.getResourceAsStream("/images/right" + i + ".png")));
+            BACK_IMAGES[i] = new Image(Objects.requireNonNull(
+                    ImageLoader.class.getResourceAsStream("/images/back" + i + ".png")));
         }
-        wall = new Image(Objects.requireNonNull(ImageLoader.class.getResourceAsStream("/images/wall1.png")));
-        floor = new Image(Objects.requireNonNull(ImageLoader.class.getResourceAsStream("/images/floor1.png")));
-        neighbor = new Image(Objects.requireNonNull(ImageLoader.class.getResourceAsStream("/images/neighbor.png")));
-  }
+        wall = new Image(Objects.requireNonNull(
+                ImageLoader.class.getResourceAsStream("/images/wall.png")));
+        floor = new Image(Objects.requireNonNull(
+                ImageLoader.class.getResourceAsStream("/images/floor.png")));
+        neighbor = new Image(Objects.requireNonNull(
+                ImageLoader.class.getResourceAsStream("/images/neighbor.png")));
+    }
 }
