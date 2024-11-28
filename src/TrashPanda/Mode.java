3325b4879d32735241c8dc8ee8a39c18ddc41a7a
Yeali Kim <ykim229@my.bcit.ca>
2024-11-28 00:38:47 -0800

    Write javadoc and adhere to checkstyle

--- src/TrashPanda/Mode.java
+++ src/TrashPanda/Mode.java
@@ -3,24 +3,61 @@ package TrashPanda;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
-public abstract class Mode {
+/**
+ * Represents an abstract class of game mode in TrashPanda game.
+ * Subclasses define different types of game modes with specific behaviors, maze sizes, and effects.
+ */
+abstract class Mode {
+    /**
+     * Size of the maze for this mode.
+     */
     protected final int mazeSize;
+    /**
+     * Difficulty level of this game mode.
+     */
     protected final String difficulty;
 
-    public Mode(String difficulty) {
+    /**
+     * Constructs a new Mode instance with the specified difficulty.
+     * Maze size is set based on the difficulty level provided.
+     *
+     * @param difficulty String that represents the level of the mode ("Easy" or "Hard")
+     * @throws IllegalArgumentException If an invalid difficulty is provided
+     */
+    Mode(final String difficulty) {
+        final int easyMaze = 5;
+        final int hardMaze = 10;
         if (difficulty.equals("Easy")) {
-            this.mazeSize = 5;
+            this.mazeSize = easyMaze;
         } else if (difficulty.equals("Hard")) {
-            this.mazeSize = 10;
+            this.mazeSize = hardMaze;
         } else {
             throw new IllegalArgumentException("Invalid difficulty: " + difficulty);
         }
         this.difficulty = difficulty;
     }
 
+    /**
+     * Gets the name of the current mode.
+     *
+     * @return String that represents the name of the mode
+     */
     public abstract String getName();
 
-    public abstract Maze createMaze();
+    /**
+     * Creates a new maze instance based on the current mode.
+     *
+     * @return Maze instance created based on the current mode.
+     */
+    abstract Maze createMaze();
 
-    public abstract void applyEffects(GraphicsContext gc, Player player, Canvas canvas, Maze maze);
+    /**
+     * Applies effects based on the current mode.
+     *
+     * @param gc     The GraphicsContext used for drawing on the canvas.
+     * @param player The current player instance.
+     * @param canvas The canvas where the effects are drawn.
+     * @param maze   The current maze instance.
+     */
+    abstract void applyEffects(GraphicsContext gc, Player player, Canvas canvas, Maze maze);
 }
